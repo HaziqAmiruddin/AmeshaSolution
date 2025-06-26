89361b39b33b13e40fb1dd6ae99af7eb41364edc
Haziq Amiruddin <141046553+HaziqAmiruddin@users.noreply.github.com>
2025-06-26 15:05:44 +0800

    check 1

--- js/photoslider.js
+++ js/photoslider.js
@@ -78,3 +78,21 @@ document.querySelectorAll(".image-slider").forEach(slider => {
       });
     });
   });
+
+  // Fullscreen function for mobile
+  function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { // Safari
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE11
+      elem.msRequestFullscreen();
+    }
+  }
+
+  // Attach to all slider images
+  document.querySelectorAll('.slider-image').forEach(img => {
+    img.addEventListener('click', () => {
+      openFullscreen(img);
+    });
+  });
